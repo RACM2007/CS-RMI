2502d13d1220fe8b928d2a96c2d27c368288832b
Hugo Angeles <hugoangeles0810@gmail.com>
2016-10-02 14:21:43 -0500

    Cambiando forma de conexioncon bd

--- server/src/db/ConnectionFactory.java
+++ server/src/db/ConnectionFactory.java
@@ -10,31 +10,30 @@ public class ConnectionFactory {
 
   private static Connection connection = null;
   private static Parametros parametros = null;
-  private static Boolean changeParametros = true;
 
   public static void setParametros(Parametros params) {
     parametros = params;
-    changeParametros = true;
   }
 
   public static Connection getConnection() {
     try {
-      if (connection == null || connection.isClosed() || changeParametros) {
-        String driver = "com.mysql.jdbc.Driver";
-        String host = parametros.getHostDB();
-        Integer port = parametros.getPortDB();
-        String user = parametros.getUserDB();
-        String pass = parametros.getPassDB();
-        String database = parametros.getNameDB();
-        String url = "jdbc:mysql://" + host + ":" + port + "/" + database + "?autoReconnect=true&useSSL=false";
 
-        Class.forName(driver);
-        connection = DriverManager.getConnection(url, user, pass);
-        changeParametros = false;
-      }
+      String driver = "com.mysql.jdbc.Driver";
+      String host = parametros.getHostDB();
+      Integer port = parametros.getPortDB();
+      String user = parametros.getUserDB();
+      String pass = parametros.getPassDB();
+      String database = parametros.getNameDB();
+      String url = "jdbc:mysql://" + host + ":" + port + "/" + database + "?autoReconnect=true&useSSL=false";
+
+      Class.forName(driver);
+      connection = DriverManager.getConnection(url, user, pass);
+
     } catch(ClassNotFoundException e) {
+      connection = null;
       e.printStackTrace();
     } catch(SQLException e) {
+      connection = null;
       e.printStackTrace();
     }
 
